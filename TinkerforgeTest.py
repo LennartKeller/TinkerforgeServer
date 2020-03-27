8dd99f28f92d43cfd252231115434a39819d9e0a
Lennart Keller <lennart.keller@googlemail.com>
2020-03-27 16:55:26 +0100

    update test script

--- TinkerforgeTest.py
+++ TinkerforgeTest.py
@@ -5,26 +5,13 @@ UID_HAT = "JXG"
 UID_WEA = "K2g"
 UID_MOT = "J9X"
 
-count = 1 # counter Schleifen
-t_count = 0 #counter Temperatur Warnung
-lon2 = 0 # longitude
-lat2 = 0 # Latitude
-d = 0 # Entfernung
-
-import requests
-import time
-import math
 
 from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_gps_v2 import BrickletGPSV2
 from tinkerforge.brick_hat import BrickHAT
 from tinkerforge.bricklet_outdoor_weather import BrickletOutdoorWeather
 from tinkerforge.bricklet_motion_detector_v2 import BrickletMotionDetectorV2
-from pushover import Client
-from requests.exceptions import Timeout
-
-# Token für pushover
-client = Client ("u9yMhUgytePtkw9W6wMjWhG6GuyFiE", api_token="aopj7djwr7degpd2vw3e8pc5gmy5aq")
+from TinkerforgeServer import ObjectServer
 
 if __name__ == "__main__":
     ipcon = IPConnection() # Create IP connection
@@ -33,3 +20,11 @@ if __name__ == "__main__":
     wea = BrickletOutdoorWeather(UID_WEA, ipcon)
     mot = BrickletMotionDetectorV2(UID_MOT, ipcon)
     ipcon.connect(HOST, PORT) # Connect to brickd
+
+    # add objects to server
+
+    server = ObjectServer()
+    server.add(gps)
+    server.add(hat)
+    server.add(wea)
+    server.add(mot)
