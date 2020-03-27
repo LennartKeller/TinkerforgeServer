e2bdf5f22fd21777879cc5435aacbcb1b13a2c70
Lennart Keller <lennart.keller@googlemail.com>
2020-03-27 18:41:32 +0100

    change attribute and method handling

--- TinkerforgeServer/objectserver.py
+++ TinkerforgeServer/objectserver.py
@@ -1,33 +1,51 @@
 from flask import Flask
 from flask import jsonify
+from typing import Tuple, List
 
 
 class ObjectServer:
 
     def __init__(self):
         self.application = Flask(__name__)
-        self.object_store = {}
+        self.objects = {}
 
-    def add(self, obj):
-        self.object_store[obj.__class__.__name__] = obj
+    def add(self, obj: object):
+        self.objects[obj.__class__.__name__] = obj
+
+    def _get_attributes(self, obj: object) -> List[str]:
+        attributes = []
+        for entry in dir(obj):
+            if not callable((getattr(obj, entry))):
+                attributes.append(entry)
+        return attributes
+
+    def _get_methods(self, obj: object) -> List[str]:
+        methods = []
+        for entry in dir(obj):
+            if callable((getattr(obj, entry))):
+                methods.append(entry)
+        return methods
 
     def _register_routes(self):
         @self.application.route('/')
-        def get_objects():
-            return {'response': list(self.object_store.keys())}, 200
+        def get_objects() -> Tuple[dict, int]:
+            return {'response': list(self.objects.keys())}, 200
 
-        @self.application.route('/<object_name>')
-        def get_object(object_name):
+        @self.application.route('/<string:object_name>')
+        def get_object(object_name: str) -> Tuple[dict, int]:
             try:
-                obj = self.object_store[object_name]
+                obj = self.objects[object_name]
+                attributes = self._get_attributes(obj)
+                methods = self._get_methods(obj)
+                return {obj.__class__.__name__: {'attributes': attributes, 'methods': methods}}, 200
             except KeyError:
                 return {'response': 'Could not find object {}'.format(object_name)}, 400
-            return {obj.__class__.__name__: dir(obj)}, 200
 
-        @self.application.route('/<object_name>/<attribute_name>')
-        def get_attribute(object_name, attribute_name):
+
+        @self.application.route('/<string:object_name>/<string:attribute_name>')
+        def get_attribute(object_name: str, attribute_name: str) -> Tuple[dict, int]:
             try:
-                obj = self.object_store[object_name]
+                obj = self.objects[object_name]
             except KeyError:
                 return {'response': 'Could not find object {}'.format(object_name)}, 400
             if not getattr(obj, attribute_name):
@@ -39,7 +57,7 @@ class ObjectServer:
             try:
                 return {'response': response}, 200
             except TypeError:
-                return {'response': str(response)}, 500
+                return {'response': str(response)}, 200
 
     def run(self, *args, **kwargs):
         self._register_routes()
