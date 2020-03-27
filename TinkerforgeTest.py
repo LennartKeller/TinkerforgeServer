77cf416e2ecd804e20aaa06361408d58438a4a95
Lennart Keller <lennart.keller@googlemail.com>
2020-03-27 17:19:38 +0100

    restructure test file

--- TinkerforgeTest.py
+++ TinkerforgeTest.py
@@ -1,3 +1,11 @@
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_gps_v2 import BrickletGPSV2
+from tinkerforge.brick_hat import BrickHAT
+from tinkerforge.bricklet_outdoor_weather import BrickletOutdoorWeather
+from tinkerforge.bricklet_motion_detector_v2 import BrickletMotionDetectorV2
+from TinkerforgeServer import ObjectServer
+
+
 HOST = "localhost"
 PORT = 4223
 UID_GPS = "GPS" # Change XYZ to the UID of your GPS Bricklet 2.0
@@ -6,13 +14,6 @@ UID_WEA = "K2g"
 UID_MOT = "J9X"
 
 
-from tinkerforge.ip_connection import IPConnection
-from tinkerforge.bricklet_gps_v2 import BrickletGPSV2
-from tinkerforge.brick_hat import BrickHAT
-from tinkerforge.bricklet_outdoor_weather import BrickletOutdoorWeather
-from tinkerforge.bricklet_motion_detector_v2 import BrickletMotionDetectorV2
-from TinkerforgeServer import ObjectServer
-
 if __name__ == "__main__":
     ipcon = IPConnection() # Create IP connection
     gps = BrickletGPSV2(UID_GPS, ipcon) # Create device objects
@@ -20,12 +21,12 @@ if __name__ == "__main__":
     wea = BrickletOutdoorWeather(UID_WEA, ipcon)
     mot = BrickletMotionDetectorV2(UID_MOT, ipcon)
     ipcon.connect(HOST, PORT) # Connect to brickd
-
-    # add objects to server
-
-    server = ObjectServer()
-    server.add(gps)
-    server.add(hat)
-    server.add(wea)
-    server.add(mot)
-    server.run(host='0.0.0.0', port=8080, debug=True)
+    if ipcon.get_connection_state():
+        server = ObjectServer()
+        server.add(gps)
+        server.add(hat)
+        server.add(wea)
+        server.add(mot)
+        server.run(host='0.0.0.0', port=8080, debug=True)
+    else:
+        print("Connection to Tinkerforge failed")
